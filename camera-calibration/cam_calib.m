b2e2270640b3d28d96476ab9c123cbb580f0c034
Jiawei Xu <cyandicnx@hotmail.com>
2017-10-07 17:56:28 -0400

    Update cam_calib.m

--- camera-calibration/cam_calib.m
+++ camera-calibration/cam_calib.m
@@ -1,11 +1,11 @@
 % ----------------- positive calculation starts here ----------------- 
-syms u v ux vx wx f p or oc Rco Pco P0A;
+syms u v ux vx wx f p Or oc Rco Pco P0A;
 Rco = sym("Rco", [3,3]);
 Pco = sym("Pco", [3,1]);
 P0A = sym("P0A", [3,1]);
 
 % res is a 3x1 matrix holding u', v' and w'
-res=[f/p, 0, or;0, f/p, oc;0,0,1]*[Rco, Pco]*[P0A;1];
+res=[f/p, 0, Or;0, f/p, oc;0,0,1]*[Rco, Pco]*[P0A;1];
 % respectively ux, vx, wx
 ux = res(1);
 vx=res(2);
